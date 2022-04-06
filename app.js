1bc4db1562a5c5b956257365a1e06d43c10e5bfe
Cigomba Miler <Cigombamiler@gmail.com>
2022-04-05 22:43:59 -0700

    styling and disabled functions

--- app.js
+++ app.js
@@ -3,25 +3,33 @@ navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || nav
 const video = document.querySelector("#video");
 const audio = document.querySelector("#audio");
 const canvas = document.querySelector("#canvas");
+const context = canvas.getContext("2d");
 
-video.height = "1280";
-
-const context = canvas.getContext("webgl2");
+const modelParams = {
+	flipHorizontal: true, // flip e.g for video
+	maxNumBoxes: 20, // maximum number of boxes to detect
+	iouThreshold: 0.5, // ioU threshold for non-max suppression
+	scoreThreshold: 0.6 // confidence threshold for predictions.
+};
 
 let model;
 
-handTrack.load().then(lmodel => {
-	model = lmodel;
-});
+function __init() {
+	handTrack.load().then(lmodel => {
+		model = lmodel;
+		console.log("Model", model);
+	});
+}
 
-function startVideo(params) {
+function startVideo() {
 	handTrack.startVideo(video).then(status => {
 		if (status) {
 			navigator.mediaDevices.getUserMedia(
 				{ video: {} },
 				stream => {
 					video.srcObject = stream;
-					console.log("stream working");
+					// setInterval(runDetection, 1000);
+					// runDetection();
 				},
 				err => {
 					console.log(err);
@@ -34,8 +42,13 @@ function startVideo(params) {
 function runDetection() {
 	model.detect(video).then(predictions => {
 		console.log(predictions);
+		model.renderPredictions(predictions, canvas, context, video);
+		if (predictions.length > 0) {
+			audio.play();
+		}
+		// requestAnimationFrame(runDetection);
 	});
 }
 
+// __init();
 startVideo();
-runDetection();
